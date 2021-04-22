f431defdd2a741b1c98e14f8940204ae8fe41a49
Heiji Morimoto <heijimrt@heijimrt.heijimrt>
2021-04-21 21:23:32 -0300

    communicating with schematics, zip and downloading

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,46 +1,70 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Header, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import shell from 'shelljs';
 const { exec } = require('child_process');
 var fs = require('fs');
 const del = require('del');
 var archiver = require('archiver');
+import { Response } from 'express';
+import { Readable } from 'stream';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
-  async generator(@Body() request): Promise<string> {
-    console.log(request);
-    /**
-     * comentado momentaneamente
-     */
-    // const dir = './angular';
-    // await del(dir);
-    // await fs.mkdirSync(dir);
-    // await process.chdir(dir);
+  @Header('Content-type', 'application/zip')
+  async generator(
+    @Body() request,
+    @Res() res: Response,
+  ){
+    const dir = '/home/heijimrt/Desktop/generator/invest';
+    await del(`${dir}/generated`, { force: true });
+    await process.chdir(dir);
+    const pages = [];
+    const type = request.mode;
 
-    // await exec('ng new test --skip-install', (err, stdout, stderr) => {
-    //   if (err) {
-    //     // node couldn't execute the command
-    //     return;
-    //   }
-    //   var zipFolder = require('zip-folder');
- 
-    //   zipFolder('./test', '../foo.zip', function(err) {
-    //       if(err) {
-    //           console.log('oh no!', err);
-    //       } else {
-    //           console.log('EXCELLENT');
-    //           process.chdir('../');
-    //       }
-    //   });
-      
-    //   console.log(`stdout: ${stdout}`);
-    //   console.log(`stderr: ${stderr}`);
-    // });
-    return request;
+    for (let feature of request.features) {
+      const page = {
+        name: feature.name,
+        artifacts: feature.artifacts,
+      };
+      pages.push(page);
+    }
+    const command = `schematics .:invest --dry-run=false --type='${type}' --pages='${JSON.stringify(pages)}'`;
+
+    await exec(command,
+      (err, stdout, stderr) => {
+        if (err) {
+          return;
+        }
+        var zipFolder = require('zip-folder');
+  
+        zipFolder(`${dir}/generated`, '../foo.zip', function(err) {
+            if(err) {
+                console.log('oh no!', err);
+            } else {
+                console.log('EXCELLENT');
+            }
+        });
+        
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+
+      const pdf = await new Promise<Buffer>((resolve, reject) => {
+        fs.readFile('../foo.zip', (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        });
+      });
+      const stream = new Readable();
+      stream.push(pdf);
+      stream.push(null);
+      stream.pipe(res);
   }
   
 }
